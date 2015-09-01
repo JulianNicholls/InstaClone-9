80588d934d755eef7ef639d84c415b246fdaf5e0
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-01 16:04:33 +0100

    Signup code is in place and working.

--- ParseStarterProject/ViewController.swift
+++ ParseStarterProject/ViewController.swift
@@ -21,25 +21,66 @@ class ViewController: UIViewController {
 
     @IBAction func signupPressed(sender: AnyObject) {
         if username.text != "" && password.text != "" {
+            showActivityIndicator()
 
+            let user = PFUser()
+
+            user.username = username.text
+            user.password = password.text
+
+            var errorMessage = "Plase try again later"
+            
+            user.signUpInBackgroundWithBlock({
+                (success, error) -> Void in
+
+                self.indicator.stopAnimating()
+                UIApplication.sharedApplication().endIgnoringInteractionEvents()
+
+                if success {
+                    // Signup successful
+                }
+                else {
+                    if let errorString = error!.userInfo["error"] as? String {
+                        errorMessage = errorString
+                    }
+
+                    self.showAlert("Error signing up", message: errorMessage)
+                }
+            })
         }
         else {
-            let alert = UIAlertController(title: "Error in Form", message: "You must enter both a username and password.", preferredStyle: .Alert)
-
-            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {
-                (action) -> Void in
-
-                self.dismissViewControllerAnimated(true, completion: nil)
-            }))
-
-            self.presentViewController(alert, animated: true, completion: nil)
+            showAlert("Error in Form", message: "You must enter both a username and password.")
         }
     }
 
     @IBAction func loginPressed(sender: AnyObject) {
     }
 
+    func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .Alert)
 
+        alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: {
+            (action) -> Void in
+
+            self.dismissViewControllerAnimated(true, completion: nil)
+        }))
+
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+
+    func showActivityIndicator() {
+        indicator = UIActivityIndicatorView(frame: CGRectMake(0, 0, 50, 50))
+
+        indicator.center = self.view.center
+        indicator.hidesWhenStopped = true
+        indicator.activityIndicatorViewStyle = .Gray
+
+        self.view.addSubview(indicator)
+
+        indicator.startAnimating()
+
+        UIApplication.sharedApplication().beginIgnoringInteractionEvents()
+    }
 
 
     override func didReceiveMemoryWarning() {
