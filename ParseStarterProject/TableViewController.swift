5c300a98d14c7464ede63cd115dea80cee8fc16c
Julian Nicholls <juliannicholls29@gmail.com>
2015-09-03 10:20:19 +0100

    Fill user table.
    The users table is filled with the user names, skipping the currently logged-in user.

---
+++ ParseStarterProject/TableViewController.swift
@@ -0,0 +1,116 @@
+//
+//  TableViewController.swift
+//  InstaClone
+//
+//  Created by Julian Nicholls on 03/09/2015.
+//  Copyright © 2015 Parse. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TableViewController: UITableViewController {
+
+    var userNames   = [""]
+    var userIDs     = [""]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let query = PFUser.query()
+
+        query?.findObjectsInBackgroundWithBlock({
+            (objects, error) -> Void in
+
+            if let users = objects {
+                self.userNames.removeAll()
+                self.userIDs.removeAll()
+
+                for object in users {
+                    if let user = object as? PFUser {
+                        if user.objectId != PFUser.currentUser()?.objectId {
+                            self.userNames.append(user.username!)
+                            self.userIDs.append(user.objectId!)
+                        }
+                    }
+                }
+            }
+
+            self.tableView.reloadData()
+
+            print(self.userNames)
+            print(self.userIDs)
+        })
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return userNames.count
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+
+        cell.textLabel?.text = userNames[indexPath.row]
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
